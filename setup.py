0d79cbd1c4dd9bd37e9e25c637bf12892e915f91
Tristan Crockett <tristan.h.crockett@gmail.com>
2016-12-21 15:29:14 -0600

    Tox/Travis/Codecov integration (#12)
    * Tox/Travis/Codecov integration
    
    * Need tox in the requirements file
    
    * Remove travis install that should be unnecessary with tox, and refactor config to support missing config file

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='tyra',
+    version='0.1.0',
+    description="Model evaluation",
+    author="Center for Data Science and Public Policy",
+    author_email='datascifellows@gmail.com',
+    url='https://github.com/dssg/tyra',
+    install_requires=[],
+    license="MIT license",
+    zip_safe=False,
+    keywords='tyra',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.4',
+    ],
+    test_suite='tests',
+)
