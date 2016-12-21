b47871ee1fb659d9ec1ff813c3c74cec33567c5d
Tristan Crockett <tristan.h.crockett@gmail.com>
2016-12-21 14:01:12 -0600

    Fill out detail of model search test

--- tests/test_evaluations_searchmodels.py
+++ tests/test_evaluations_searchmodels.py
@@ -7,12 +7,18 @@ models_data = [
     # model_id, run_time, model_type
     (1, CUTOFF + timedelta(days=1), 'a_model_type'),
     (2, CUTOFF - timedelta(days=1), 'a_model_type'),
+    (3, CUTOFF + timedelta(days=1), 'a_model_type'),
 ]
 
 evaluations_data = [
     # model_id, metric, parameter, value
     (1, 'recall@', '5.0', 45),
-    (2, 'recall@', '5.0', 55)
+    (1, 'recall@', '10.0', 46),
+    (1, 'precision@', '5.0', 47),
+    (1, 'precision@', '10.0', 48),
+    (2, 'recall@', '5.0', 55),
+    (3, 'recall@', '5.0', 60),
+    (3, 'precision@', '10.0', 50),
 ]
 
 data = {
@@ -26,7 +32,13 @@ def test_search_models():
         route = '/evaluations/search_models'
         response = test_app.post(
             route,
-            data=dict(metric1='recall', parameter1='5.0', timestamp=CUTOFF)
+            data=dict(
+                metric1='recall',
+                parameter1='5.0',
+                metric2='precision',
+                parameter2='10.0',
+                timestamp=CUTOFF
+            )
         )
         assert response.status_code == 200
         response_data = json.loads(response.get_data().decode('utf-8'))
