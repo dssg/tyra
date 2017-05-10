5d8eb27db0d347fc992bf9843257928d0c636018
tweddielin <power6188@gmail.com>
2017-05-10 17:29:39 -0500

    add login manager features in the backend by flask_login

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,10 +2,17 @@ from flask import Flask
 from webapp.flask_util_js import FlaskUtilJs
 from flask_sqlalchemy import SQLAlchemy
 from config import dburl
+import flask_login
 
 app = Flask(__name__,instance_relative_config=True)
+app.secret_key = "tyra american top models"
 app.config['SQLALCHEMY_DATABASE_URI'] = dburl
 db = SQLAlchemy(app)
 fujs = FlaskUtilJs(app)
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+users = {'power6188@gmail.com': {'pw': 'power6188'}}
 
 from webapp import controller, query
