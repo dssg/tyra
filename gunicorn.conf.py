096017153676be4d6c71b6ede78c2acf89728cd7
tweddielin <power6188@gmail.com>
2017-05-15 12:46:10 -0500

    correct bash styling issue

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,7 @@
 workers = 1
 timeout = 120
 loglevel = 'debug'
-name = 'tyra' 
+name = 'tyra'
 accesslog = '-'
-access_log_format = '"%({X-Real-IP}i)s" %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+access_log_format = '"%({X-Real-IP}i)s" %(l)s %(u)s %(t)s \
+                    "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
