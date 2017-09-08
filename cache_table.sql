53de60aebd319557667690a85b99c296f49607f3
Adolfo De Unánue <nanounanue@users.noreply.github.com>
2017-09-08 14:43:40 -0500

    A better cache sql
    It includes all the metrics, not only `precision@100`

--- cache_table.sql
+++ cache_table.sql
@@ -1,26 +1,22 @@
-create materialized view results.ranked_table
-  as
-    SELECT
-      r_table.model_group_id,
-      r_table.metric_parameter,
-      avg(r_table.value),
-      max(run_time) run_time,
-      model_comment
-    FROM (
-    SELECT
+CREATE MATERIALIZED VIEW results.ranked_table
+AS
+SELECT
+r_table.model_group_id,
+r_table.metric_parameter,
+avg(r_table.value),
+max(run_time) run_time,
+model_comment
+FROM (
+      SELECT
       model_group_id,
       e.value,
       e.evaluation_start_time ::DATE,
       metric || parameter as metric_parameter,
       m.run_time,
       m.model_comment
-    FROM results.models AS m
-      JOIN results.evaluations e USING (model_id)
-    WHERE evaluation_start_time = train_end_time :: TIMESTAMP
-      AND parameter =  '100_abs'
-      AND metric = 'precision@'
-      /*AND model_comment = 'with accident as adverse'*/
+      FROM results.models AS m
+      JOIN results.evaluations AS e USING (model_id)
+      WHERE evaluation_start_time = train_end_time :: TIMESTAMP
       ) r_table
-    GROUP BY r_table.model_group_id, r_table.metric_parameter, r_table.model_comment
-    ORDER BY avg DESC;
-
+GROUP BY r_table.model_group_id, r_table.metric_parameter, r_table.model_comment
+ORDER BY avg DESC;
